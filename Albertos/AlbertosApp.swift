f653c1fbd8c5c20676674ccd1a520c1a7e413bcf
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-08-22 17:30:07 -0500

    Initial Commit

---
+++ Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 22/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Albertos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Albertos
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 22/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
