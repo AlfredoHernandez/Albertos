fc22de321b147f77610fe8fb2b6586810d40a59e
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-08-23 13:51:39 -0500

    Replace menu placeholder with fetcher

--- Albertos/AlbertosApp.swift
+++ Albertos/AlbertosApp.swift
@@ -10,30 +10,9 @@ struct AlbertosApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                MenuList(viewModel: .init(menuFetcher: MenuFetcherPlaceholder()))
+                MenuList(viewModel: .init(menuFetcher: MenuFetcher()))
                     .navigationTitle("Alberto's 🇮🇹")
             }
         }
-    }
-}
-
-let menu: [MenuItem] = [
-    MenuItem(category: "starters", name: "Caprese Salad", spicy: false, price: 0.0),
-    MenuItem(category: "starters", name: "Arancini Balls", spicy: false, price: 0.0),
-    MenuItem(category: "pastas", name: "Penne all'Arrabbiata", spicy: true, price: 0.0),
-    MenuItem(category: "pastas", name: "Spaghetti Carbonara", spicy: false, price: 0.0),
-    MenuItem(category: "drinks", name: "Water", spicy: false, price: 0.0),
-    MenuItem(category: "drinks", name: "Red Wine", spicy: false, price: 0.0),
-    MenuItem(category: "desserts", name: "Tiramisù", spicy: false, price: 0.0),
-    MenuItem(category: "desserts", name: "Crema Catalana", spicy: false, price: 0.0),
-]
-
-class MenuFetcherPlaceholder: MenuFetching {
-    func fetchMenu() -> AnyPublisher<[MenuItem], Error> {
-        Future { completion in
-            completion(.success(menu))
-        }
-        // .delay(for: 0.5, scheduler: RunLoop.main)
-        .eraseToAnyPublisher()
     }
 }
