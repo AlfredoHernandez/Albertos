d0d5d3a98875e2baabd68168ffa7c4aa0dc1272f
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-09-07 16:10:55 -0500

    Update `MenuItemDetailView` UI

--- Albertos/Menu Item Detail UI/MenuItemDetailView.swift
+++ Albertos/Menu Item Detail UI/MenuItemDetailView.swift
@@ -8,22 +8,31 @@ struct MenuItemDetailView: View {
     @ObservedObject private(set) var viewModel: MenuItemDetailViewModel
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
+        VStack(alignment: .leading) {
             Text(viewModel.name)
-                .fontWeight(.bold)
-
+                .font(.title)
             if let spicy = viewModel.spicy {
-                Text(spicy)
-                    .font(Font.body.italic())
+                HStack {
+                    Text(spicy).font(Font.body.bold())
+                    Text("🔥")
+                }
             }
-
             Text(viewModel.price)
-            Button(viewModel.addOrRemoveFromOrderButtonText) {
-                viewModel.addOrRemoveFromOrder()
-            }
+            Stepper(
+                viewModel.addOrRemoveFromOrderButtonText,
+                onIncrement: {
+                    viewModel.addOrRemoveFromOrder()
+                },
+                onDecrement: {}
+            )
+            Divider()
+            Text("Description")
+                .bold()
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            )
             Spacer()
-        }
-        .padding(8)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        }.padding(.horizontal)
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
